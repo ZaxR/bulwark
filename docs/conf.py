e089f9aba77c82579bfe688bb9ea89eebbd4520d
Zax Rosenberg <zax.rosenberg@gmail.com>
2019-08-06 10:18:48 -0500

    chg conf.py source_suffix to specify bother .rst and .md

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,10 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ['.rst', ]
+source_suffix = source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = 'index'
